e1d07ddff1b31342dd8aac8ba324c08fac973a32
Kuldeep Jadhav <kuldeep.jadhav@live.com>
2015-12-09 17:35:12 +0530

    Inital commit for Learning typescript

---
+++ Edge.ts
@@ -0,0 +1,11 @@
+import VertexMod = require('./Vertex');
+export class Edge {
+	vertexFrom: VertexMod.Vertex;
+	vertexTo: VertexMod.Vertex;
+	distance: number;
+	constructor(from, to, distance) {
+		this.vertexFrom = from;
+		this.vertexTo = to;
+		this.distance = distance;
+	} 
+}
